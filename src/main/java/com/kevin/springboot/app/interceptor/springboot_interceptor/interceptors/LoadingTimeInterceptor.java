db5c0d27e357e77c03c646361a6f9cd67b02d856
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2024-12-11 11:50:02 -0500

    Feat: Registrando interceptor en configuración spring

--- src/main/java/com/kevin/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/kevin/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,26 +1,32 @@
 package com.kevin.springboot.app.interceptor.springboot_interceptor.interceptors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component("timeInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor{
 
+
+    private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
-        // TODO Auto-generated method stub
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        logger.info("LoadingTimeInterceptor: postHandle() saliendo...");
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
-        // TODO Auto-generated method stub
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+            throws Exception {    
+        
+        logger.info("LoadingTimeInterceptor: preHandle() entrando...");    
+        return true;
     }
 
     
