c3557a2043752cee852bafd8f88b6677fe9b36c6
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2024-12-11 10:33:04 -0500

    Feat: Creando Proyecto

---
+++ src/main/java/com/kevin/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -0,0 +1,27 @@
+package com.kevin.springboot.app.interceptor.springboot_interceptor.interceptors;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoadingTimeInterceptor implements HandlerInterceptor{
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+        // TODO Auto-generated method stub
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        // TODO Auto-generated method stub
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    
+}
